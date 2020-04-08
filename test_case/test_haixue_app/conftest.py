e287881f00a69173db1f7a4c75e1ff1d00b9f1a3
tangkun4379@haixue.com <tangkun4379@haixue.com>
2020-04-08 22:33:14 +0800

    修复jenkins传参带有引号问题---

--- test_case/test_haixue_app/conftest.py
+++ test_case/test_haixue_app/conftest.py
@@ -48,6 +48,8 @@ def get_appium_driver():
             print(type(get_value("desired_caps")))
             print(eval(get_value("desired_caps")))
             print(type(eval(get_value("desired_caps"))))
+
+
             if globle_arg["pattern"] == "local":
                 driver = AppiumDriver(url="http://127.0.0.1:4723/wd/hub",
                                       platform_type=platform_type,
